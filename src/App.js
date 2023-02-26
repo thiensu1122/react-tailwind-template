4571ff9bd25772b574a7b34fc5e6b67b399807d0
thiensu1122 <thiensuhoisinh2007@gmail.com>
2023-02-25 23:27:58 -0800

    - Working

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
     return (
         <div>
-            Hello
+            <h1 className='text-3xl font-bold underline'> Hello</h1>
         </div>
     )
 }
